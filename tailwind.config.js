8f1ca86fb4496fa303a687825501263237100ec1
Michael Johnston <lastobelus@mac.com>
2020-02-06 20:41:00 -0800

    convert to tailwind up to end of section 5
    rename .scss files to css
    use as many tailwind styles in html as possible
    use @apply for btn styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@ module.exports = {
   theme: {
     extend: {
       height: {
-        v95: '95vh'
+        v95: "95vh",
+        "14": "3.5rem"
+      },
+      fontSize: {
+        "7xl": "5rem",
+        "8xl": "6rem",
       }
-    },
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
